d138ff8a311064d8f9f15fad0b821e428803dc7a
Jirka Marsik <jiri.marsik@oracle.com>
2017-05-24 18:12:13 +0200

    Add a volume bar

--- taffybar.hs
+++ taffybar.hs
@@ -5,7 +5,9 @@ import System.Taffybar.TaffyPager
 import System.Taffybar.SimpleClock
 import System.Taffybar.MPRIS2
 
+import System.Taffybar.Widgets.PollingBar
 import System.Taffybar.Widgets.PollingGraph
+import System.Taffybar.Widgets.PollingLabel
 
 import System.Information.Memory
 import System.Information.CPU
@@ -24,6 +26,15 @@ cpuCallback = do
   (userLoad, systemLoad, totalLoad) <- cpuLoad
   return [totalLoad, systemLoad]
 
+volCallback :: IO Double
+volCallback = do
+  muteFile <- readFile "/home/jirka/.mute"
+  let muteNum = fromMaybe 0 $ readMaybe muteFile
+  let mute = if muteNum == 0 then False else True
+  volumeFile <- readFile "/home/jirka/.volume"
+  let volumeAbs = fromMaybe 0 $ readMaybe volumeFile
+  return $ if mute then 0 else volumeAbs / 90000
+
 fgcolor :: String -> String -> String
 fgcolor color contents =
   "<span fgcolor='" ++ color ++ "'>" ++ contents ++ "</span>"
@@ -39,16 +50,18 @@ main = do
                                                       ]
                                   , graphLabel = Just "cpu"
                                   }
+      volCfg = defaultBarConfig $ const (52 / 255, 187 / 255, 249 / 255)
   let clock = textClockNew Nothing (fgcolor "orange" "%a %b %_d %H:%M") 1
       pager = taffyPagerNew $ defaultPagerConfig { emptyWorkspace = fgcolor "gray" }
       mpris = mpris2New
       mem = pollingGraphNew memCfg 1 memCallback
       cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
+      vol = pollingBarNew volCfg 1 volCallback
       tray = systrayNew
       chosenMonitor = fromMaybe 0 $ readMaybe $ head args
       myTaffybarConfig = defaultTaffybarConfig { startWidgets = [ pager ]
                                                , endWidgets = (if chosenMonitor == 0 then [ tray ] else [])
-                                                              ++ [ clock, mem, cpu, mpris ]
+                                                              ++ [ clock, mem, cpu, vol, mpris ]
                                                , monitorNumber = chosenMonitor
                                                }
   defaultTaffybar myTaffybarConfig
